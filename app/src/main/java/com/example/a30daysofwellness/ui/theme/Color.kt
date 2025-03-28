0b22073de15b12acac2c766ddb203c6275db305f
SaharRamezani <saharramezanijolfaie@gmail.com>
2025-03-28 16:55:06 +0100

    init (after icon and theme selection)

---
+++ app/src/main/java/com/example/a30daysofwellness/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.a30daysofwellness.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val LightBlue = Color(0xFF5AA9E6)
+val SoftBlue = Color(0xFF7FC8F8)
+val AlmostWhite = Color(0xFFF9F9F9)
+val YellowAccent = Color(0xFFFFE45E)
+val PinkAccent = Color(0xFFFF6392)
+
+// Dark Theme Colors
+val DarkBackground = Color(0xFF1A1C26)
+val DarkBlue = Color(0xFF314459)
+val DarkGray = Color(0xFF2F2F38)
+val Mocha = Color(0xFF7B645A)
+val DeepBrown = Color(0xFF2B1D21)
